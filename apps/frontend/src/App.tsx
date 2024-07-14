58904a35e6b502469ad274f567a1550644bdd85d
Puskar-Roy <puskarroy600@gmail.com>
2024-07-14 16:18:28 +0530

    feat : profile set

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
+import ProfileSetup from "./components/ProfileSetup";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           element={<Home />}
         />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={ <ProfileSetup/> }/>
       </Routes>
 
     </>
