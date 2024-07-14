7e8146f1fa099cd0cce910a266143710c13a2596
shuvadeepmondal <121492365+shuvadeepmondal@users.noreply.github.com>
2024-07-14 16:27:40 +0530

    Login Page Updated

--- apps/frontend/src/components/Login.tsx
+++ apps/frontend/src/components/Login.tsx
@@ -17,40 +17,52 @@ export default function Login(): JSX.Element {
     setPassword(event.target.value);
   };
 
+  const handleClick = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
   return (
     <main className="w-full h-screen flex ">
-      <div className="grid grid-cols-1  md:grid-cols-2 m-auto h-[350px] sm:max-w-[800px] rounded-2xl">
+      <div className="grid grid-cols-1  md:grid-cols-2 m-auto h-[450px] sm:max-w-[850px] rounded-2xl">
         <div className="w-full h-[300px] hidden md:block">
-          <img className="" src={log_img} alt="" />
+          <img className="mt-14" src={log_img} alt="" />
         </div>
-        <div className="p-4 flex flex-col justify-around">
-        <h1 className="text-5xl ml-8 font-semibold poppins-semibold bg-blue-500 text-transparent bg-clip-text">Welcome!</h1>
-        <p className="font-medium rubik text-indigo-300 ">Welcome back ! Please enter Your details.</p>
+        <div className="p-5 flex flex-col justify-around">
+        <form onSubmit={handleClick}>
+        <h1 className="text-5xl ml-[110px] poppins-semibold text-slate-900 mb-3 ">Login</h1>
             <div>
+              <label className="poppins-medium ml-1 text-slate-600">Email Address</label>
               <input
-                className="border p-2 w-full rounded-xl bg-slate-100 "
+                className="border px-3 py-2 w-[350px] rounded-xl bg-slate-100 mt-2"
                 type="text"
-                placeholder="Email"
+                placeholder="example@gmail.com"
                 value={email}
                 onChange={handleEmailChange}
               />
-            </div>
+             </div>
             <div>
+              <label className="poppins-medium ml-1 text-slate-600">Password</label>
               <input
-                className="border p-2 w-full rounded-xl bg-slate-100 focus:ring-1 focus:border-none ring-rose-700"
+                className="border px-3 py-2 w-[350px] rounded-xl bg-slate-100 mt-2"
                 type="password"
-                placeholder="Password"
+                placeholder="Your Password"
                 value={password}
                 onChange={handlePasswordChange}
               />
             </div>
-
+            <div className=" ml-44">
+                       <button className='ml-10 font-medium text-base text-blue-500'>Forgot Password?</button>
+                   </div>
             <button
-              className=" ml-9 w-[230px] active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all p-2.5 rounded-3xl bg-gradient-to-r from-violet-700 to-blue-500  text-white text-lg font-semibold poppins-extrabold"
+              className=" ml-14 w-[230px] active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all p-2.5 rounded-3xl bg-gradient-to-r from-violet-700 to-blue-500  text-white text-lg font-semibold poppins-extrabold"
               key="1"
-            ><p>Login</p>
+            ><p>Log in</p>
             </button>
-            </div>
+                <div className='mt-2 flex ml-[50px]'>
+                    <p className="font-medium text-base"> Did not have an Account ?</p>
+                    <button className="text-blue-500 text-base font-medium ml-2 ">Register</button>
+               </div>
+        </form>
+   </div>
 
       </div>
     </main>
