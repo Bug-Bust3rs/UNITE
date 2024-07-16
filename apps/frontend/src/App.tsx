e7e76ec644a9c4f19304ec9373d77e737fe39079
shuvadeepmondal <121492365+shuvadeepmondal@users.noreply.github.com>
2024-07-17 01:10:23 +0530

    OTP Page Added

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Login from "./components/Login";
 import ProfileSetup from "./components/ProfileSetup";
 import Register from "./components/Register";
 import ForgotPassword from "./components/Forgot-Password";
+import OTP from "./components/OTP";
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path="/register" element={ <Register/> }/>
         <Route path="/profile" element={ <ProfileSetup/> } />
         <Route path="/forgot-password" element={ <ForgotPassword/> } />
+        <Route path="/otp" element={ <OTP/> } />
 
       
       </Routes>
