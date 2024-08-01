0c67d76324a176227296efbcd575c2cc82111f47
Puskar-Roy <puskarroy600@gmail.com>
2024-08-01 23:40:34 +0530

    chore : profile

--- apps/frontend/src/components/ui/Navbar.tsx
+++ apps/frontend/src/components/ui/Navbar.tsx
@@ -63,8 +63,6 @@ const Navbar: React.FC = () => {
                 <Link to="/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Hire Skilled Workers</Link>
               </li>
 
-              {state.user && <li className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                <Link to={'/profile'}>{state.user.name}</Link></li>}
               <li>
                 <button
                   onClick={toggleTheme}
