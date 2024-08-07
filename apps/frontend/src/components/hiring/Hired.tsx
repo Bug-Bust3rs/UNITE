d6f0f6dcff52c3ee38c4964c26b37d82bdc853ff
shuvadeepmondal <121492365+shuvadeepmondal@users.noreply.github.com>
2024-08-08 00:35:23 +0530

    Hire Page Ui Updated

--- apps/frontend/src/components/hiring/Hired.tsx
+++ apps/frontend/src/components/hiring/Hired.tsx
@@ -1,15 +1,16 @@
+import Adv from "./Adv"
 import Searchbar from "./Search"
 // import Appoitment from "./Appoitment"
 
 
 export default function hired() {
   return (
-    <div className=" pt-14 mt-[100px] lg:mt-0 lg:pt-32 bg-[url('https://pagedone.io/asset/uploads/1691055810.png')] dark:bg-slate-900  bg-cover">
+    <div className=" pt-14 pb-24 mt-[100px] lg:mt-0 lg:pt-32 bg-[url('https://pagedone.io/asset/uploads/1691055810.png')] dark:bg-slate-900  bg-cover">
         <div className="">
             <Searchbar/>
         </div>
         <div>
-
+         <Adv/>
         </div>
     </div>
   )
