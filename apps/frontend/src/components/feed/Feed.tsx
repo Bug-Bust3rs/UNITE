f8d3e407df10486523eefe7659d8930d9369b748
shuvadeepmondal <121492365+shuvadeepmondal@users.noreply.github.com>
2024-11-06 11:34:45 +0530

    fix: Bug

--- apps/frontend/src/components/feed/Feed.tsx
+++ apps/frontend/src/components/feed/Feed.tsx
@@ -5,15 +5,14 @@ import MainFeed from "./MainFeed"
 const Feed = () => {
     return (
 
-        <div className="flex flex-col-reverse items-start lg:flex-row w-auto lg:w-[80%] mx-auto pt-10 lg:pt-[100px] xl:gap-5 2xl:gap-0 min-h-screen ">
-
+        <div className="flex flex-col-reverse items-start lg:flex-row w-auto lg:w-[80%] mx-auto pt-10 lg:pt-[100px] xl:gap-5 2xl:gap-0 min-h-screen">
             <AsideLeft />
             {/* xl:ml-[23rem] */}
-            <main className="w-[88%] flex flex-col gap-10 lg:w-[50%]  mx-auto  ">
+            <main className="w-[95%] flex flex-col gap-10 lg:w-[45%]  mx-auto  ">
                 <MainFeed />
                 <MainFeed />
 
-                <MainFeed />
+                <MainFeed />  
                 <MainFeed />
 
                 <MainFeed />
@@ -29,4 +28,4 @@ const Feed = () => {
     )
 }
 
-export default Feed
+export default Feed;
